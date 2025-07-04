ba64762a66bc6c5b8f02aefbd7614d119c0e38e7
Juan Nícolas <juannicolasdev@gmail.com>
2025-07-04 15:07:27 -0300

    feat: criando DTO e Mapper do missoes

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
@@ -0,0 +1,27 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setDificuldade(missoesDTO.getNinjas().toString());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel) {
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setDificuldade(missoesModel.getNinjas().toString());
+
+        return missoesDTO;
+    }
+}
